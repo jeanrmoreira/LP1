6b6348a7c4a467b0c51e9bc752d394bb0d752884
jeanrmoreira <48541470+jeanrmoreira@users.noreply.github.com>
2019-05-10 21:27:46 -0300

    Create 1.c

---
+++ Exercicios/lp1-07/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct Personagem {
+     int forca;
+     int energia;
+     int experiencia;
+};
+
+void main (void){
+     struct Personagem p1;
+     p1.forca = 10;
+     p1.energia = 50;
+     p1.experiencia = 30;
+     
+     struct Personagem p2 = {18, 20, 25};       
+}
